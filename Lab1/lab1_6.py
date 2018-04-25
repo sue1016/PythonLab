bd56ce52532a440544cf5c12cbee9e6dbd0b4247
Cara Sue Ubuntu <skx15826273720@gmail.com>
2018-04-25 20:26:15 +0800

    lab1_6 done

--- Lab1/lab1_6.py
+++ Lab1/lab1_6.py
@@ -1,2 +1,28 @@
 #oral area
+import random
 
+longAxis = 10
+shortAxis = 8
+
+def getAreaOfOral(longAxis,shortAxis,dotsNum):
+    countAll = 0
+    countIn = 0
+    halfLongAxis = 0.5 * longAxis
+    halfShortAxis = 0.5 * shortAxis
+    while countAll < dotsNum:
+        randomX = random.random() * halfLongAxis
+        randomY = random.random() * halfShortAxis
+        y = pow((1 - pow(randomX,2)/pow(halfLongAxis,2)) * pow(halfShortAxis,2),0.5)
+        if randomY < y:
+            countIn = countIn + 1
+        countAll = countAll + 1
+    squareArea = halfLongAxis * halfShortAxis
+    oralRatio = countIn / countAll
+    oralArea = (squareArea * oralRatio) * 4
+    return oralArea
+
+precise = 1
+initDotsNum = 1000
+while precise < 100000000:
+    print(getAreaOfOral(10,8,initDotsNum * precise))
+    precise = precise * 10
