a24c97bcaed6a7ef3b4b801e22539fdc30f209fa
sue1016 <15826273720@163.com>
2018-06-11 16:40:00 +0800

    lab3 knn add ref

--- Lab3/knn.py
+++ Lab3/knn.py
@@ -75,4 +75,11 @@ print(knn(20))
 #选取与当前点距离最小的K个点
 #确定前K个点所在类别的出现频率
 #返回前k个点出现频率最高的类别最为当前点的预测分类
-#k表示用于选择最近邻的数目           
+#k表示用于选择最近邻的数目          
+# 
+# [ref:读写CSV数据](http://python3-cookbook-personal.readthedocs.io/zh_CN/latest/c06/p01_read_write_csv_data.html)
+# [ref:计算向量欧氏距离](https://blog.csdn.net/sscssz/article/details/52456848)
+# [ref:knn](https://github.com/frankstar007/kNN/blob/master/python%E6%9C%BA%E5%99%A8%E5%AD%A6%E4%B9%A0%E4%B9%8B%20K-%E9%82%BB%E8%BF%91%E7%AE%97%E6%B3%95.md)
+# [ref:使用lambda应对各种复杂情况的排序，包括list嵌套dict](https://www.polarxiong.com/archives/Python-%E4%BD%BF%E7%94%A8lambda%E5%BA%94%E5%AF%B9%E5%90%84%E7%A7%8D%E5%A4%8D%E6%9D%82%E6%83%85%E5%86%B5%E7%9A%84%E6%8E%92%E5%BA%8F-%E5%8C%85%E6%8B%AClist%E5%B5%8C%E5%A5%97dict.html)
+# [ref:统计列表中的重复项出现的次数](https://site.douban.com/136082/widget/notes/6031056/note/215926715/)
+# [ref:How do I sort a dictionary by value?](https://stackoverflow.com/questions/613183/how-do-i-sort-a-dictionary-by-value)
